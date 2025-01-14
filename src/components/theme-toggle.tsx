52f6dc57c8c86a358b6e1e20f2c26a569a1e0192
AnasShahid <anasshahid88@gmail.com>
2025-01-14 11:26:47 +0500

    Theme toggle work

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -21,15 +21,15 @@ export function ThemeToggle() {
 
   return (
     <Button
-      variant="ghost"
+      variant="default"
       size="icon"
       onClick={handleClick}
       className="relative w-9 h-9"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0 text-orange-500" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-slate-900 dark:text-slate-100" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0 text-slate-100 dark:text-slate-100" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-slate-900 dark:text-slate-900" />
       <Monitor 
-        className={`absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all text-slate-900 dark:text-slate-100 ${
+        className={`absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all text-slate-900 dark:text-slate-900 ${
           theme === "system" ? "!rotate-0 !scale-100" : ""
         }`}
       />
