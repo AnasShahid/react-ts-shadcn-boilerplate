3520e7bd94307bdbaa99a94d724ce52bce2c3d72
AnasShahid <anasshahid88@gmail.com>
2025-01-15 18:50:42 +0500

    Corrected the application structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { useEnv } from './hooks/use-env';
 import { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
   const env = useEnv();
@@ -12,7 +14,11 @@ function App() {
     console.log('App Title:', env.APP_TITLE);
   }, [env]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import { router } from './router';
 import './i18n';
 import './index.css';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
